d4dd1686fded5afdac10c972edd54835a06e7d89
Sadaf Patwekar <SadafP@citiustech.com>
2019-09-25 12:42:52 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/details', (req, res, next) => {
+    const date = new Date();
+    const time = date.toLocaleTimeString();
+    
+    res.locals.time = time;
+    next();
+});
+
+app.use('/details', (req, res) => {
+    const time = res.locals.time;
+    res.send(
+        '<div style="text-align: center;">' +
+            '<p>' +
+                '<b>' + 'Current Time:' + '</b>' +
+                '<br/>' + time +
+            '</p>' +
+        '</div>'
+    );
+});
+
+app.use('/', (req, res) => {
+    res.send(
+        '<div>' +
+            '<a href="/home">Home</a><br/>' + 
+            '<a href="/details">Details</a><br/>' +
+        '</div>'
+    );
+});
+
+app.listen(3000);
